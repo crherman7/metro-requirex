c02723bb117997d45472ec0024cac0e239aca89a
Chris Herman <crherman7@gmail.com>
2025-03-11 19:47:25 -0400

    feat(nx): init nx repo

---
+++ libs/metro-requirex/src/lib/metro-requirex.spec.ts
@@ -0,0 +1,7 @@
+import { metroRequirex } from './metro-requirex';
+
+describe('metroRequirex', () => {
+  it('should work', () => {
+    expect(metroRequirex()).toEqual('metro-requirex');
+  });
+});
---
+++ libs/metro-requirex/src/lib/metro-requirex.ts
@@ -0,0 +1,3 @@
+export function metroRequirex(): string {
+  return 'metro-requirex';
+}
