6d43a18f3f5cf01299a19e0bd726465fd867165a
Chris Herman <crherman7@gmail.com>
2025-03-11 20:42:55 -0400

    fix(docs): add global doc

--- libs/metro-requirex/src/@types/global.d.ts
+++ libs/metro-requirex/src/@types/global.d.ts
@@ -1,3 +1,15 @@
+/**
+ * Declares a global variable `__r` which is a function that takes a module ID as a parameter
+ * and returns an unknown type. This is typically used internally by the Metro bundler.
+ *
+ * The Metro bundler is a JavaScript bundler commonly used with React Native. It uses the `__r`
+ * function to require modules by their ID during the bundling process.
+ *
+ * @global
+ * @var __r
+ * @param {number} moduleId - The ID of the module to be required.
+ * @returns {unknown} The module that corresponds to the given module ID.
+ */
 declare global {
   // eslint-disable-next-line no-var
   var __r: (moduleId: number) => unknown;
